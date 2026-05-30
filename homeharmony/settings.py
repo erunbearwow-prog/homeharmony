b263422a8a8834118abd81a61bdde03e6ab8cea4
Dmitry H. Shabaykin <shabaykin.d@gmail.com>
2026-05-30 18:15:19 +0300

    add kitchen app urls add HH urls add kitchen index.html template

--- homeharmony/settings.py
+++ homeharmony/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c!wqv+n@i50ryz#-wbzv=9$n%&h&p%s(#75iirl9oqyqv%@m_8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kitchen.apps.KitchenConfig',
 ]
 
 MIDDLEWARE = [
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
 
 # Create your views here.
+def index(request):
+    t = render_to_string('kitchen/index.html')
+    return HttpResponse(t)
