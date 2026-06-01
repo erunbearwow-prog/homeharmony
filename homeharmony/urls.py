86822daf2530cb67ed70c7cc8f8e2ca2cd98b866
Dmitry H. Shabaykin <shabaykin.d@gmail.com>
2026-06-01 17:12:21 +0300

    add recipe moved recipe_detail to the root of kitchen

--- homeharmony/urls.py
+++ homeharmony/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,3 +38,7 @@ urlpatterns = [
     path('contacts/', TemplateView.as_view(template_name='contacts.html'), name='contacts'),
     path('faq/', TemplateView.as_view(template_name='faq.html'), name='faq'),
 ]
+
+# Добавьте это для обслуживания медиафайлов в режиме разработки
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
